7e9a23fb9b91cb8f3b876e4ea97c87df8f46c450
enenkoff <enenkoff@mail.ru>
2018-06-04 01:42:12 +0300

    SVG:figures appear

--- exp-svg-animate/dev/js/main.js
+++ exp-svg-animate/dev/js/main.js
@@ -1,8 +1,25 @@
 (function () {
 
-    var path = document.getElementById('path03');
-    var length = path.getTotalLength();
+    var path = document.getElementById('path03'),
+        hand01 = document.getElementById('hand01'),
+        hand02 = document.getElementById('hand02'),
+        hand03 = document.getElementById('hand03'),
+        hand04 = document.getElementById('hand04'),
+        hand05 = document.getElementById('hand05');
 
-    alert(length)
+
+    var length = path.getTotalLength(),
+        length_hand01 = hand01.getTotalLength(),
+        length_hand02 = hand02.getTotalLength(),
+        length_hand03 = hand02.getTotalLength(),
+        length_hand04 = hand02.getTotalLength(),
+        length_hand05 = hand02.getTotalLength();
+
+    // alert(length)
+    // alert(length_hand01)
+    // alert(length_hand02)
+    // alert(length_hand03)
+    // alert(length_hand04)
+
 
 })();
